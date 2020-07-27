ffefddbb2ed00eb024c9a6f8549873919ade328b
rgrlinux <rgrlinux@gmail.com>
2020-07-26 22:36:21 -0300

    fim 5

--- core/views.py
+++ core/views.py
@@ -43,9 +43,11 @@ def login_page(request):
         password = form.cleaned_data.get('password')
         user = authenticate(request, username=username, password=password)
         print(user)
+        print(request.user.is_authenticated)
 
         if user is not None:
-            #login(user.user)
+            print(request.user.is_authenticated)
+            login(request, user)
             print('Login válido')
             return redirect('/')
         else:
