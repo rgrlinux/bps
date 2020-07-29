25dbb65a4045f45d3d714500d5f720264dcc3906
rgrlinux <rgrlinux@gmail.com>
2020-07-29 19:38:58 -0300

    add template

--- products/views.py
+++ products/views.py
@@ -6,12 +6,13 @@ from .models import Product
 # Class Based view
 class ProductListView(ListView):
     queryset = Product.objects.all()
+    template_name = 'list.html'
 
 
 # Function based view
 def product_list_view(request):
     queryset = Product.objects.all()
     context = {
-        'qs':queryset
+        'qs': queryset
     }
-    return render(request, 'products/list.html', context)
+    return render(request, 'list.html', context)
