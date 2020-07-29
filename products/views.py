82a86782b922db89056045d7e851d8c71d8e2ca5
rgrlinux <rgrlinux@gmail.com>
2020-07-29 19:57:55 -0300

    ajuste dos objetos

--- products/views.py
+++ products/views.py
@@ -8,11 +8,16 @@ class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = 'list.html'
 
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
+
 
 # Function based view
 def product_list_view(request):
     queryset = Product.objects.all()
     context = {
-        'qs': queryset
+        'object_list': queryset
     }
     return render(request, 'list.html', context)
