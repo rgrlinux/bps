c307f6f884d88cce4146866f17919fd877b75026
rgrlinux <rgrlinux@gmail.com>
2020-08-09 14:15:15 -0300

    templates adicionados

--- products/views.py
+++ products/views.py
@@ -4,15 +4,22 @@ from django.views.generic import ListView, DetailView
 from .models import Product
 
 
+class ProductFeaturedListView(ListView):
+    template_name = 'list.html'
+
+    def get_queryset(self):
+        return Product.objects.featured()
+
+
+class ProductFeaturedDetailView(DetailView):
+    template_name = 'featured-detail.html'
+    queryset = Product.objects.all().featured()
+
+
 # Class Based view
 class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = 'list.html'
-
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
-    #     print(context)
-    #     return context
 
 
 # Function based view
