bd7448bf14e5b1fb4c3775c24751658e52d69581
rgrlinux <rgrlinux@gmail.com>
2020-08-09 16:12:35 -0300

    add blank to slug field

--- products/models.py
+++ products/models.py
@@ -31,7 +31,7 @@ class ProductManager(models.Manager):
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(blank=True, unique=True)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=100)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
@@ -43,8 +43,10 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
-    def product_pre_save_receiver(self, sender, instance, *args, **kwargs):
-        if not instance.slug:
-            instance.slug = unique_slug_generator(instance)
 
-    pre_save.connect(product_pre_save_receiver, sender=Product)
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+
+pre_save.connect(product_pre_save_receiver, sender=Product)
