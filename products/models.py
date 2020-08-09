7329000f6f99740f38fdd822fea8119d2155bf3e
rgrlinux <rgrlinux@gmail.com>
2020-08-09 13:47:49 -0300

    ajustes do models.py

--- products/models.py
+++ products/models.py
@@ -1,9 +1,27 @@
 from django.db import models
 
 
+# Custom query set
+class ProductQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+    def featured(self):
+        return self.filter(featured=True, active=True)
+
+
 class ProductManager(models.Manager):
-    def get_by_id(self, id):
-        qs = self.get_queryset().filter(id=id)
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def all(self):
+        return self.get_queryset().active()
+
+    def featured(self):
+        return self.get_queryset().featured()
+
+    def get_by_id(self, pk):
+        qs = self.get_queryset().filter(id=pk)
         if qs.count() == 1:
             return qs.first()
         return None
@@ -14,6 +32,8 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=100)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
+    featured = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
 
     objects = ProductManager()
 
