1bf4fbb97dc922bd69caa1fdcd4d9611b04eb15d
rgrlinux <rgrlinux@gmail.com>
2020-08-14 09:47:08 -0300

    add reverse

--- bichanopreto/urls.py
+++ bichanopreto/urls.py
@@ -22,11 +22,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('products/', include('products.urls'))
+    path('products/', include('products.urls', namespace='products'))
 
 ]
 if settings.DEBUG:
-    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import *
 
+app_name = 'products'
 
 urlpatterns = [
     path('', ProductListView.as_view()),
-    path('<slug:slug>/', ProductDetailSlugView.as_view()),
+    path('<slug:slug>/', ProductDetailSlugView.as_view(), name='detail'),
 ]
